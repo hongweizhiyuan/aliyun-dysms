1662beaf634aaae0cf9585b9dee524e4ed6f1072
hongweizhiyuan <403236160@qq.com>
2019-02-26 10:44:20 +0800

    案例的自动加载

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+//自动加载方法
+function classLoader($class)
+{
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $path = str_replace('Yanghongwei\AliyunSms' . DIRECTORY_SEPARATOR, '', $path);
+
+    $file = __DIR__ . '/src/' . $path . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('classLoader');
