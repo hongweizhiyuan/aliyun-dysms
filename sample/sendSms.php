48cd6684b4c09c6639f786d06e4d5ea5ba374538
hongweizhiyuan <403236160@qq.com>
2019-02-26 08:04:28 +0800

    remove id

--- sample/sendSms.php
+++ sample/sendSms.php
@@ -8,13 +8,13 @@ if (is_file(__DIR__ . '/../autoload.php')) {
 use Aliyun\Sms;
 
 //参数
-$accessKeyId = 'LTAIrvHQztFVfYzL';
-$accessKeySecret = 'rcGXcujTVYbJQAjP5j5dC4kWfeAGFM';
-$signName = '山西岐伯';
-$templateCode = 'SMS_158480026';
+$accessKeyId     = 'xxx';
+$accessKeySecret = 'xxx';
+$signName        = 'xxx';
+$templateCode    = 'xxx';
 
 //实例化并发送
 $fun = new Sms($accessKeyId, $accessKeySecret, $signName, $templateCode);
-$result = $fun->sendSms('18903467858', '1234');
+$result = $fun->sendSms('1890346xxxx', '1234');
 print_r($result);
 
