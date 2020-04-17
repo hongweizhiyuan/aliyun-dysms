8227f31d67c973cda5917837b18cee6a42741b53
hongweizhiyuan <403236160@qq.com>
2020-04-17 17:45:40 +0800

    fix

--- sample/sendSms.php
+++ sample/sendSms.php
@@ -8,14 +8,14 @@ if (is_file(__DIR__ . '/../autoload.php')) {
 
 
 //参数
-$accessKeyId     = 'LTAIrvHQztFVfYzL';
-$accessKeySecret = 'rcGXcujTVYbJQAjP5j5dC4kWfeAGFM';
-$signName        = '山西岐伯';
-$templateCode    = 'SMS_100985112';
+$accessKeyId     = 'xxx';
+$accessKeySecret = 'xxx';
+$signName        = 'xxx';
+$templateCode    = 'SMS_123456';
 
 //实例化并发送
 $fun = new \Yanghongwei\AliyunSms\Sms($accessKeyId, $accessKeySecret, $signName, $templateCode);
-$result = $fun->sendSms('18903467858', '1234');
+$result = $fun->sendSms('18912345678', '1234');
 print_r($result);
 print_r($result);
 
